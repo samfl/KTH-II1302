a9f50583b44aa9e77f07da794e6a972a1457aef8
Love Pelz <grodanlove@gmail.com>
2019-04-09 09:31:50 +0200

    try to get app to start

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var ws = require("socket.io")(http);
 var cfenv = require('cfenv');
-var IoTApp  = require('./application/application.js');
+//var IoTApp  = require('./application/application.js');
 
 /* Serve the files out of ./public as our main files. */
 app.use(express.static(__dirname + '/public'));
@@ -18,14 +18,14 @@ var app_env = cfenv.getAppEnv({vcapFile: 'vcap.json'});
 const IOT_PLATFORM = "iotex";
 
 /* Retrieve Cloud Foundry environment variables. */
-var credentials = app_env.getServiceCreds(IOT_PLATFORM);
-var application = new IoTApp(credentials.org, credentials.apiKey, credentials.apiToken);
+//var credentials = app_env.getServiceCreds(IOT_PLATFORM);
+//var application = new IoTApp(credentials.org, credentials.apiKey, credentials.apiToken);
 
 /* Application is an event emitter, so we listen for the payload event we defined in application.js! */
-application.on('payload', function(data) {
+//application.on('payload', function(data) {
   /* We then broadcast to our clients.  */
-  ws.emit('broadcast', JSON.parse(data).temp);
-});
+ // ws.emit('broadcast', JSON.parse(data).temp);
+//});
 
 /* Start server on the specified port and binding host app_env.port */
-http.listen(app_env.port || 4096, function() {});
+http.listen(4096, function() {});
