ef3504db1f863fc44db748728e731720324efa54
albroms <albroms@kth.se>
2019-04-09 15:57:25 +0200

    added app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+//var http = require('http').Server(app);
+//var ws = require("socket.io")(http);
+var cfenv = require('cfenv');
+require('./public/js/page-routing.js')(app);
+//var IoTApp  = require('./application/application.js');
+
+/* Serve the files out of ./public as our main files. */
+app.use(express.static(__dirname + '/public'));
+
+
+//const IOT_PLATFORM = "iotex";
+var app_env = cfenv.getAppEnv();
+
+app.listen(app_env.port, '0.0.0.0', function() {
+});
+
+/* Retrieve Cloud Foundry environment variables. */
+//var credentials = app_env.getServiceCreds(IOT_PLATFORM);
+//var application = new IoTApp(credentials.org, credentials.apiKey, credentials.apiToken);
