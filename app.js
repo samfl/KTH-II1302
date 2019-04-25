8faae1f766f42792866c3011708663543810406d
Sam Florin <sam.florin@hotmail.com>
2019-04-25 15:55:27 +0200

    das

--- app.js
+++ app.js
@@ -11,15 +11,19 @@ var Client = require('ibmiotf').IotfApplication;
 var iotCredentials;
 
 //const IOT_PLATFORM = "iotex";
+//OK
 var app_env = cfenv.getAppEnv();
 
 //Loop through configuration internally defined in Bluemix and retrieve the credential from the IoT service
+//OK
 var baseConfig = app_env.getServices('iotex');
 iotCredentials = baseConfig['iotex'];
 
 /* Serve the files out of ./public as our main files. */
+//OK
 app.use(express.static(__dirname + '/public'));
 
+//OK
 var iotAppConfig = {
  "org" : iotCredentials.credentials.org,
  "id" : iotCredentials.credentials.iotCredentialsIdentifier,
@@ -28,26 +32,29 @@ var iotAppConfig = {
  "auth-token" : iotCredentials.credentials.apiToken
 }
 
+//OK
 var appClient = new Client(iotAppConfig);
 appClient.connect();
 console.log("Successfully connected to our IoT service!");
 
 
 // subscribe to input events
+// OK
 appClient.on("connect", function () {
  console.log("subscribe to input events");
  appClient.subscribeToDeviceEvents("g10-pi");
 });
 
+//OK
 var motionSensorData = {"motionPayload":{}};
 
 // deviceType "raspberrypi" and eventType "motionSensor" are published by client.py on RaspberryPi
-/*
+
 appClient.on("deviceEvent", function(deviceType, deviceId, eventType, format, payload){
  if (eventType === 'motionSensor'){
    motionSensorData.motionPayload = JSON.parse(payload);
  }
-}  */
+}
 
 app.listen(app_env.port, '0.0.0.0', function() {
 });
