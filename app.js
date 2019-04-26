29792dfdb4c5369d8ad7851b7ce2912145db7925
Sam Florin <sam.florin@hotmail.com>
2019-04-26 10:56:32 +0200

    sadsa

--- app.js
+++ app.js
@@ -3,82 +3,29 @@ var app = express();
 var http = require('http').Server(app);
 var ws = require("socket.io")(http);
 var cfenv = require('cfenv');
-require('./public/js/login.js')(app);
-require('./public/js/page-routing.js')(app);
-//var IoTApp  = require('./application/application.js');
-var Client = require('ibmiotf').IotfApplication;
-var EventEmitter = require('events');
-
-var iotCredentials;
-
-//const IOT_PLATFORM = "iotex";
-//OK
-var app_env = cfenv.getAppEnv();
-
-//Loop through configuration internally defined in Bluemix and retrieve the credential from the IoT service
-//OK
-var baseConfig = app_env.getServices('iotex');
-iotCredentials = baseConfig['iotex'];
+var IoTApp  = require('./application.js');
 
 /* Serve the files out of ./public as our main files. */
-//OK
 app.use(express.static(__dirname + '/public'));
 
-//OK
-var iotAppConfig = {
- "org" : iotCredentials.credentials.org,
- "id" : iotCredentials.credentials.iotCredentialsIdentifier,
- "auth-method" : "apikey",
- "auth-key" : iotCredentials.credentials.apiKey,
- "auth-token" : iotCredentials.credentials.apiToken
-}
-
-//OK
-var appClient = new Client(iotAppConfig);
-appClient.connect();
-console.log("Successfully connected to our IoT service!");
-
-
-// subscribe to input events
-// OK
-var ee = new EventEmitter();
-
-appClient.on("connect", function () {
- console.log("subscribe to input events");
- appClient.subscribeToDeviceEvents("g10-pi");
-
- ee.appClient.on("deviceEvent", async function (deviceType, deviceId, eventType, format, payload) {
-    //console.log("Device Event from :: " +deviceType + " : " + deviceId + " of event " + eventType + " with payload : " + payload);
-    ee.emit('payload', payload);
-  });
-});
-
-//OK
-//var motionSensorData = {"motionPayload":{}};
-
-
-
-// deviceType "raspberrypi" and eventType "motionSensor" are published by client.py on RaspberryPi
-//FUNKAR EJ!
 /*
-appClient.on("deviceEvent", function(deviceType, deviceId, eventType, format, payload){
- if (eventType === 'motionSensor'){
-   motionSensorData.motionPayload = JSON.parse(payload);
- }
-}*/
+  Get the app environment from Cloud Foundry,
+  if you are developing locally (VCAP_SERVICES environment variable not set),
+  cfenv will use the file defined by vcapFile instead.
+  You can export these local json files from IBM Cloud!
+*/
+var app_env = cfenv.getAppEnv({vcapFile: 'vcap.json'});
+const IOT_PLATFORM = "iotex";
 
+/* Retrieve Cloud Foundry environment variables. */
+var credentials = app_env.getServiceCreds(IOT_PLATFORM);
+var application = new IoTApp(credentials.org, credentials.apiKey, credentials.apiToken);
 
-ee.on('payload', function(data) {
+/* Application is an event emitter, so we listen for the payload event we defined in application.js! */
+application.on('payload', function(data) {
   /* We then broadcast to our clients.  */
   ws.emit('broadcast', JSON.parse(data));
 });
 
-//OK
-app.listen(app_env.port, '0.0.0.0', function() {
-});
 /* Start server on the specified port and binding host app_env.port */
-//http.listen(app_env.port, function() {});
-
-/* Retrieve Cloud Foundry environment variables. */
-//var credentials = app_env.getServiceCreds(IOT_PLATFORM);
-//var application = new IoTApp(credentials.org, credentials.apiKey, credentials.apiToken);
+http.listen(app_env.port || 4096, function() {});
