d2bbc36e2c5d18995f1159c48c6c6a2eabe262a7
Love Pelz <grodanlove@gmail.com>
2019-05-06 12:34:28 +0200

    .

--- public/js/alarm.js
+++ public/js/alarm.js
@@ -1,6 +1,9 @@
+var ibmdb = require('ibm_db');
 
 module.exports = function(app){
 
+ibmdb.open("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net;UID=ldk15513;PWD=hqsv1nbr3^7tdzg1;PORT=50000;PROTOCOL=TCPIP", function (err,conn) {
+ if (err) return console.log(err);
 
  app.post('/setAlarm', function(request, response) {
   
@@ -21,44 +24,10 @@ module.exports = function(app){
 	});
 });
 
-application.on('payload', function(data) {
-
-//function checkForAlarm() {
-  if(data === 1){
-    var date = new Date();
-    var currentHour = String(date.getHours()).padStart(2, "0");
-    var currentMin = String(date.getMinutes()).padStart(2, "0");
-    var currentSec = String(date.getSeconds()).padStart(2, "0");
-    var currentTime = currentHour + ":" + currentMin;
-   
-    var query = 'SELECT * from ALARMS WHERE START < CAST(? as time) AND END > CAST(? as time)'
-    conn.queryResult(query, [currentTime,currentTime], function (err, result) {
-
-        var re = result.fetchSync();
-    
-         if(err) { console.log(err); }
-            else{
-                if(re===null){
-                }
-                else{	
-                currentTime = currentTime + ':' + currentSec;
-                var currentDate = date.toISOString().slice(0,10);
-                
-                var queryin = "INSERT INTO EVENTS (EVENTTIME,EVENTDATE) VALUES (?, ?)";
-
-	            conn.queryResult(queryin, [currentTime,currentDate], function (err, result) {
-	           	if(err) { 
-                         console.log(err);
-              }
-	            });
-
-              }
-          }
-        });
-      }
-    });
 
 
 
+
+});
 
 }
